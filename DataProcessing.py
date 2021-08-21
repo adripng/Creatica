96a7f83a39a77e4bc3924feb72658914a62e40fc
adriannap <adri.peng@gmail.com>
2021-08-21 17:42:31 -0400

    data processing updates

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,1 +1,31 @@
+import numpy as np
+import csv
+import pickle
 
+parks = "parks.csv"
+species = "species.csv"
+
+parkRows = {}
+with open(parks, 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    parkFields = next(csvreader)
+
+    for row in csvreader:
+        parkRows[row[0]] = row[1:]
+        # row format: [CODE, NAME, STATE, SIZE(ACRES), LONGITUDE, LATITUDE]
+
+speciesRows = []
+parkAnimals = {}
+with open(species, 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    speciesFields = next(csvreader)
+
+    for row in csvreader:
+        speciesRows.append(row)
+        # row format: [CODE, PARK NAME, CATEGORY, ORDER, FAMILY, SCIENTIFIC NAME, COMMON NAME, 
+        # RECORD STATUS, OCCURENCE, NATIVENESS, ABUNDANCE, SEASONALITY, CONSERVATION STATUS]
+
+print(parkFields)
+print(parkRows)
+print(speciesFields)
+print(speciesRows[:100])
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Creatica
 Creatica hackathon project
 
-we can add more to this later.
+File Guide: 
+    - parks.csv/species.csv:
+        Taken from https://www.kaggle.com/nationalparkservice/park-biodiversity. They contain information about each animal species in a national park, including its endangered status. 
+    - DataProcessing.py: 
+        File where most of the csv processing (into workable databases) will occur.
