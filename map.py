37543279129df66644118868640063f06e3f8a32
adriannap <adri.peng@gmail.com>
2021-08-22 11:57:24 -0400

    yeahhhhhh

--- map.py
+++ map.py
@@ -1,5 +1,6 @@
 import json
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 import pickle
 import random
@@ -21,14 +22,14 @@ for state in database:
     total += len(database[state])
     if "locations" not in data:
         data["locations"] = []
-        data["nums"] = []
+        data["density"] = []
     else:
         data["locations"].append(state)
-        data["nums"].append(len(database[state]) / total * 100)
+        data["density"].append(len(database[state]) / total * 100)
 
 #print(data)
 # database is currently organized as
-# {locations: states, nums: density (of endangered animals)}
+# {locations: states, density: density (of endangered animals)}
 data["text"] = []
 for i, state in enumerate(database):
     if i == 0:
@@ -36,38 +37,66 @@ for i, state in enumerate(database):
     print(i, state)
     r = random.randrange(len(database[state]))
     randomInfo = database[state][r]
-    intro = "Name: " + randomInfo[4] + "\nScientific Name: " + randomInfo[3] + "\nType: " + randomInfo[0] + "\nOrder: " + randomInfo[1] + "\nFamily: " + randomInfo[2] + "\nStatus: " + randomInfo[5] + "\n\n"
+    intro = "Name: " + randomInfo[4] + "<br>Scientific Name: " + randomInfo[3] + "<br>Type: " + randomInfo[0] + "<br>Order: " + randomInfo[1] + "<br>Family: " + randomInfo[2] + "<br>Status: " + randomInfo[5] + "<br><br>"
     out = wikipedia.search(randomInfo[3])
     while len(out) < 4 or len(randomInfo[3]) == 1 or randomInfo[3] == "Canis lupus":
         r = random.randrange(len(database[state]))
         randomInfo = database[state][r]
-        intro = "Name: " + randomInfo[4] + "\nScientific Name: " + randomInfo[3] + "\nType: " + randomInfo[0] + "\nOrder: " + randomInfo[1] + "\nFamily: " + randomInfo[2] + "\nStatus: " + randomInfo[5] + "\n\n"
+        intro = "Name: " + randomInfo[4] + "<br>Scientific Name: " + randomInfo[3] + "<br>Type: " + randomInfo[0] + "<br>Order: " + randomInfo[1] + "<br>Family: " + randomInfo[2] + "<br>Status: " + randomInfo[5] + "<br><br>"
         out = wikipedia.search(randomInfo[3])
-    print(out, randomInfo[3])
-    intro += wikipedia.summary(randomInfo[3], sentences=4, auto_suggest=False)
-    print("found")
+    #print(out, randomInfo[3])
+    try: 
+        summary = wikipedia.summary(randomInfo[3], sentences=4, auto_suggest=False)
+        print("found")
+        words = summary.split(" ")
+        print(words)
+        n = 20 # chunk length
+        chunks = [words[i:i+n] for i in range(0, len(words), n)]
+        joined = [" ".join(chunk) for chunk in chunks]
+        summary = "<br>".join(joined)
+        print(summary)
+        intro += summary
+    except: 
+        intro += "No information was found on Wikipedia for this species."
     data["text"].append(intro)
 
 # use the wikipedia module (https://stackoverflow.com/questions/4460921/extract-the-first-paragraph-from-a-wikipedia-article-python)
+# for i, state in enumerate(database):
+#     if i == 0:
+#         continue
+#     str = "hey how are you doing today i'm doing good what about you yeah i'm doing fine"
+#     words = str.split(" ")
+#     n = 4
+#     chunks = [words[i:i+n] for i in range(0, len(words), n)]
+#     joined = [" ".join(chunk) for chunk in chunks]
+#     summary = "<br>".join(joined)
+#     print(summary)
+#     data["text"].append(summary)
 
 print("text", len(data["text"]))
 print("locations", len(data["locations"]))
-print("nums", len(data["nums"]))
+print("density", len(data["density"]))
 df = pd.DataFrame(data)
 print(df)
 
-fig = px.choropleth(df, geojson=states, locations='locations', locationmode="USA-states", color='nums',
+fig = px.choropleth(df, geojson=states, locations='locations', locationmode="USA-states", color='density',
     color_continuous_scale="tealrose",
     range_color=(0, 15),
     hover_name='locations',
-    hover_data = ['text'],
+    hover_data=['text'],
+    custom_data=['locations', 'text'],
     scope="usa",
-    labels={'unemp':'unemployment rate'},
-    title=""               
+    #title="Threatened or Endangered Species in National Parks Across the US"               
     )
-fig.update_traces(marker_line_width=0, marker_opacity=0.8)
+fig.update_traces(marker_line_width=0,
+    hovertemplate="<br>".join([
+        '<b>%{customdata[0]}</b>',
+        '%{customdata[1]}'
+    ]),
+    marker_opacity=0.8)
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 fig.update_geos(
 showsubunits=True, subunitcolor="black"
 )
+fig.write_html("map.html")
 fig.show()
